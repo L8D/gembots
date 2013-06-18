a0ee7a6889cdb7f1e965eb494379a8c7ac86b9e7
L8D <tenorbiel@gmail.com>
2013-06-18 11:24:50 -0500

    Added players hash

--- lib/gembots/arena.rb
+++ lib/gembots/arena.rb
@@ -1,9 +1,15 @@
 require 'gembots'
 
 class Gembots::Arena
+  attr_reader :players
+
   def initialize *bots
+    # hash table of the ids of every player in the arena
+    @players = Hash.new
+
     bots.each do |bot|
       bot.arena = self
+      @players[bot.id] = bot
       def bot.update arena
         arena.update_bot self
       end
