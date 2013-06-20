bff061b72d0f2008096869dca42ce44bc210757b
L8D <tenorbiel@gmail.com>
2013-06-19 22:13:49 -0500

    Added support for spawning projectiles
    also changed the 'players' hash to being called 'objects'

--- lib/gembots/arena.rb
+++ lib/gembots/arena.rb
@@ -2,16 +2,16 @@ require 'gembots'
 
 # The Arena class is used to create and simulate arenas.
 class Gembots::Arena
-  # This is a hash table containing the ids of each player.
-  attr_reader :players
+  # This is a hash table containing the ids of each object.
+  attr_reader :objects
 
   def initialize *bots
-    @players = Hash.new
+    @objects = Hash.new
 
     # define each bots' update function and add to players hash
     bots.each do |bot|
       bot.arena = self
-      @players[bot.id] = bot
+      @objects[bot.id] = bot
 
       def bot.update arena
         arena.update_bot self
@@ -22,5 +22,9 @@ class Gembots::Arena
   # Used for activating the robot's custom functions based on active events.
   def update_bot robot
     # something here
+  end
+
+  def spawn object
+    @objects[object.id] = object
   end
 end
