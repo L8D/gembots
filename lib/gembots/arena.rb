eff7c9e3d07e2894c7922d49bcee10ea87d5c9b9
L8D <tenorbiel@gmail.com>
2013-06-19 22:24:36 -0500

    Added new 2D board system for grid

--- lib/gembots/arena.rb
+++ lib/gembots/arena.rb
@@ -5,13 +5,19 @@ class Gembots::Arena
   # This is a hash table containing the ids of each object.
   attr_reader :objects
 
+  # 2-dimensional array of the current arena.
+  # By default this is a 20x20 board
+  attr_reader :board
+
   def initialize *bots
     @objects = Hash.new
+    @board   = Array.new 20, (Array.new 20, [])
 
     # define each bots' update function and add to players hash
     bots.each do |bot|
       bot.arena = self
       @objects[bot.id] = bot
+      @board[bot.x_pos][bot.y_pos] << bot.id
 
       def bot.update arena
         arena.update_bot self
@@ -26,5 +32,6 @@ class Gembots::Arena
 
   def spawn object
     @objects[object.id] = object
+    @board[object.x_pos][object.y_pos] << object.id
   end
 end
