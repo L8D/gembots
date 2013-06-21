e6eb6a2434fedfdaccf7fc4ed547357abb1eadc5
L8D <tenorbiel@gmail.com>
2013-06-21 00:34:08 -0500

    Added new objects_pre and made update_bot work

--- lib/gembots/arena.rb
+++ lib/gembots/arena.rb
@@ -5,11 +5,15 @@ class Gembots::Arena
   # This is a hash table containing the ids of each object.
   attr_reader :objects
 
+  # Same as objects, but it contains and older copy to detect changes during a update_bot.
+  attr_reader :objects_pre
+
   # 2-dimensional array of the current arena.
   # By default this is a 20x20 board
   attr_reader :board
 
   def initialize *bots
+    @objects = Hash.new
     @objects = Hash.new
     @board   = Array.new 20, (Array.new 20, [])
 
@@ -17,6 +21,7 @@ class Gembots::Arena
     bots.each do |bot|
       bot.arena = self
       @objects[bot.id] = bot
+      @objects_pre[bot.id] = bot
       @board[bot.x_pos][bot.y_pos] << bot.id
 
       def bot.update arena
@@ -25,9 +30,18 @@ class Gembots::Arena
     end
   end
 
-  # Used for activating the robot's custom functions based on active events.
+  # Used for updating the board based on changes in robot.
   def update_bot robot
-    # something here
+    id = robot.id
+    # detect movement
+    if robot.x_pos != @objects_pre[id].x_pos or robot.y_pos != @objects_pre[id].y_pos
+      # set new pos
+      @board[robot.x_pos][robot.y_pos] << robot.id
+      # remove id from old pos
+      @board[@objects_pre[id].x_pos][@objects_pre[id].y_pos].delete robot.id
+    end
+    # update objects_pre
+    @objects_pre[id] = robot
   end
 
   # Spawn object into board and objects array.
